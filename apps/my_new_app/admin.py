3bc0de0f5391fd5dd5c51a9a0d3ed88ba81c974f
TemirbolatMaratuly <snake2009003@mail.ru>
2022-02-07 23:16:46 +0600

    Added Student and Group fields and Admin registration with some conditions

--- apps/my_new_app/admin.py
+++ apps/my_new_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import (Account,
+                     Student,
+                     Group
+                    )
 
 from typing import Optional
 
@@ -10,7 +13,7 @@ class AccountAdmin(admin.ModelAdmin):
     # readonly_fields = (
     #     'description',
     # )
-    list_display: tuple = ('user','full_name','description',)
+    # list_display: tuple = ('user','full_name','description',)
     
     def get_readonly_fields(self, request: HttpRequest, 
                             obj: Optional[Account] = None) -> tuple:
@@ -18,7 +21,30 @@ class AccountAdmin(admin.ModelAdmin):
             return self.readonly_fields + ('description',)
         return self.readonly_fields
 
+class StudentAdmin(admin.ModelAdmin):
+    
+    MAX_STUDENT_EDITABLE_AGE = 16
+    
+    def student_edit_age_validator(self,
+                                   obj: Optional[Student]) -> bool:
+        validator_result: bool = (obj and obj.age > self.MAX_STUDENT_EDITABLE_AGE)
+        return validator_result
+    
+    def get_readonly_fields(self, 
+                            request: HttpRequest, 
+                            obj: Optional[Student]) -> tuple:
+        if(self.student_edit_age_validator(obj)):
+            return self.readonly_fields + ('age',)
+        return self.readonly_fields
+        
 
 admin.site.register(
     Account,AccountAdmin
+)
+admin.site.register(
+    Student,StudentAdmin
+)
+
+admin.site.register(
+    Group
 )
--- apps/my_new_app/models.py
+++ apps/my_new_app/models.py
@@ -1,7 +1,9 @@
+from pyexpat import model
 from statistics import mode
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class Account(models.Model):
     user = models.OneToOneField(
@@ -22,3 +24,59 @@ class Account(models.Model):
     
     def __str__(self) -> str:
         return f'Аккаунт: {self.user_id} / {self.full_name}'
+
+class Group(models.Model):
+    GROUP_NAME_MAX_LENGTH = 10
+    name = models.CharField(
+        max_length=GROUP_NAME_MAX_LENGTH
+    )
+    
+    def __str__(self) -> str:
+        return f'Группа: {self.name}'
+    
+    class Meta:
+        ordering = (
+            'name',
+        )
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+class Student(models.Model):
+    MAX_REGISTER_AGE = 30
+    account = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE
+    )
+    age = models.IntegerField(
+        'Возраст студента'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.PROTECT
+    )
+    gpa = models.FloatField(
+        'Среднее значение GPA'
+    )
+    
+    def __str__(self) -> str:
+        return f'Student: {self.account}, Age: {self.age},\
+            Group: {self.group}, GPA: {self.gpa}'
+    
+    def save(self,*args,**kwargs) -> None:
+        if(self.age > self.MAX_REGISTER_AGE):
+            raise ValidationError('Ваш возраст должен \
+быть не более %(max_age)s лет',
+                code = 'max_possible_age',
+                params={'max_age':self.MAX_REGISTER_AGE}
+                                  )
+        super().save(args,kwargs)
+            
+    
+    class Meta:
+        ordering = (
+            'gpa',
+        )
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+    
+        
